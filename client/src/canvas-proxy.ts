888ba934b0707b3183fa9e8623a1874b673a46f4
oriaca <oriaca372m@outlook.jp>
2021-03-25 17:01:53 +0900

    クライアントのファイル分割

---
+++ client/src/canvas-proxy.ts
@@ -0,0 +1,80 @@
+import { CanvasProxy, CanvasProxyFactory, Size } from 'common'
+
+export class OffscreenCanvasProxy implements CanvasProxy {
+	constructor(private readonly _canvas: OffscreenCanvas) {}
+
+	getContext(): CanvasRenderingContext2D {
+		const ctx = this._canvas.getContext('2d')
+
+		if (ctx === null) {
+			throw new Error('Could not get a context of the OffscreenCanvas.')
+		}
+
+		return (ctx as unknown) as CanvasRenderingContext2D
+	}
+
+	drawSelfTo(ctx: CanvasRenderingContext2D): void {
+		ctx.drawImage(this._canvas, 0, 0)
+	}
+
+	async serialize(): Promise<Uint8Array> {
+		const blob = await this._canvas.convertToBlob()
+		const arrayBuffer = await blob.arrayBuffer()
+		return new Uint8Array(arrayBuffer)
+	}
+
+	get size(): Size {
+		return { width: this._canvas.width, height: this._canvas.height }
+	}
+}
+
+export class OffscreenCanvasProxyFactory implements CanvasProxyFactory {
+	createCanvasProxy(size: Size): CanvasProxy {
+		const canvas = new OffscreenCanvas(size.width, size.height)
+		return new OffscreenCanvasProxy(canvas)
+	}
+
+	async createCanvasProxyFromBitmap(data: Uint8Array): Promise<CanvasProxy> {
+		const image = await createImageBitmap(new Blob([data]))
+		const canvas = new OffscreenCanvas(image.width, image.height)
+		const renderer = canvas.getContext('2d')
+		renderer?.drawImage(image, 0, 0)
+		return new OffscreenCanvasProxy(canvas)
+	}
+}
+
+export class WebCanvasProxy implements CanvasProxy {
+	constructor(private readonly _canvas: HTMLCanvasElement) {}
+
+	getContext(): CanvasRenderingContext2D {
+		const ctx = this._canvas.getContext('2d')
+
+		if (ctx === null) {
+			throw new Error('Could not get a context of the Canvas.')
+		}
+		return (ctx as unknown) as CanvasRenderingContext2D
+	}
+
+	drawSelfTo(ctx: CanvasRenderingContext2D): void {
+		ctx.drawImage(this._canvas, 0, 0)
+	}
+
+	async serialize(): Promise<Uint8Array> {
+		return new Promise((resolve, reject) => {
+			this._canvas.toBlob((blob) => {
+				if (blob === null) {
+					reject()
+					return
+				}
+
+				blob.arrayBuffer()
+					.then((x) => resolve(new Uint8Array(x)))
+					.catch((x) => reject(x))
+			})
+		})
+	}
+
+	get size(): Size {
+		return { width: this._canvas.width, height: this._canvas.height }
+	}
+}
---
+++ client/src/paint-tool.ts
@@ -0,0 +1,117 @@
+import { ImageCanvasDrawer, LayerDrawCommand, Position, Color } from 'common'
+import { App } from './main'
+
+export interface PaintTool {
+	enable(): void
+	disable(): void
+
+	readonly isEnabled: boolean
+}
+
+export class PenTool implements PaintTool {
+	private _pathPositions: Position[] | undefined
+	color: Color = '#ff0000'
+	width = 10
+	mode: 'stroke' | 'erase' = 'stroke'
+	private readonly _imageCanvas: ImageCanvasDrawer
+	private readonly _canvasElm: HTMLCanvasElement
+
+	private _isEnabled = false
+
+	constructor(private readonly _app: App) {
+		this._imageCanvas = _app.imageCanvas
+		this._canvasElm = _app.canvasElm
+
+		this._canvasElm.addEventListener('mousedown', (e) => {
+			if (!this._isEnabled) {
+				return
+			}
+			this._startStroke(this._getPosFromEvent(e))
+		})
+
+		this._canvasElm.addEventListener('mousemove', (e) => {
+			if (!this._isEnabled) {
+				return
+			}
+			this._continueStroke(this._getPosFromEvent(e))
+		})
+
+		this._canvasElm.addEventListener('mouseup', () => {
+			if (!this._isEnabled) {
+				return
+			}
+			this._finishStroke()
+		})
+	}
+
+	enable(): void {
+		this._isEnabled = true
+	}
+
+	disable(): void {
+		this._finishStroke()
+		this._isEnabled = false
+	}
+
+	get isEnabled(): boolean {
+		return this._isEnabled
+	}
+
+	private _startStroke(pos: Position) {
+		if (this._pathPositions !== undefined) {
+			return
+		}
+
+		this._pathPositions = [pos]
+		this._imageCanvas.startPreview(this._app.selectedLayerId)
+	}
+
+	private _continueStroke({ x, y }: Position) {
+		if (this._pathPositions === undefined) {
+			return
+		}
+
+		const lastPosition = this._pathPositions[this._pathPositions.length - 1]
+		if (lastPosition.x === x && lastPosition.y === y) {
+			return
+		}
+
+		this._pathPositions.push({ x, y })
+		this._imageCanvas.drawPreview(this._constructCommand())
+		this._app.render()
+	}
+
+	private _finishStroke() {
+		if (this._pathPositions === undefined) {
+			return
+		}
+
+		this._imageCanvas.endPreview()
+		this._app.render()
+
+		this._app.commandSender.command({
+			kind: 'drawLayer',
+			layer: this._app.selectedLayerId,
+			drawCommand: this._constructCommand(),
+		})
+
+		this._pathPositions = undefined
+	}
+
+	private _getPosFromEvent(e: MouseEvent): Position {
+		const rect = this._canvasElm.getBoundingClientRect()
+		const x = e.clientX - rect.left
+		const y = e.clientY - rect.top
+
+		return { x, y }
+	}
+
+	private _constructCommand(): LayerDrawCommand {
+		return {
+			kind: this.mode,
+			positions: this._pathPositions!,
+			color: this.color,
+			width: this.width,
+		}
+	}
+}
