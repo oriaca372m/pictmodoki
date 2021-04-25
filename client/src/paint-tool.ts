def3759ba64d7274436eb96c96ec1386d8522c16
oriaca <oriaca372m@outlook.jp>
2021-04-25 21:45:36 +0900

    painttool整理

--- client/src/paint-tool.ts
+++ client/src/paint-tool.ts
@@ -15,30 +15,45 @@ export interface PaintTool {
 	readonly isEnabled: boolean
 }
 
-export class PenTool implements PaintTool {
-	private _pathPositions: Position[] | undefined
-	color: Color = '#ff0000'
-	width = 10
-	private readonly _imageCanvas: ImageCanvasDrawerWithPreview
-	private _targetLayerId: LayerId | undefined
-
+abstract class PaintToolBase implements PaintTool {
 	private _isEnabled = false
-
-	constructor(private readonly _app: PaintApp) {
-		this._imageCanvas = _app.imageCanvas
-	}
 
 	enable(): void {
 		this._isEnabled = true
 	}
 
 	disable(): void {
-		this._finishStroke()
 		this._isEnabled = false
 	}
 
 	get isEnabled(): boolean {
 		return this._isEnabled
+	}
+
+	onMouseDown(_pos: Position): void {
+		// pass
+	}
+	onMouseMoved(_pos: Position): void {
+		// pass
+	}
+	onMouseUp(_pos: Position): void {
+		// pass
+	}
+}
+
+abstract class DrawingToolBase extends PaintToolBase {
+	protected _pathPositions: Position[] | undefined
+	private readonly _imageCanvas: ImageCanvasDrawerWithPreview
+	private _targetLayerId: LayerId | undefined
+
+	constructor(private readonly _app: PaintApp) {
+		super()
+		this._imageCanvas = _app.imageCanvas
+	}
+
+	disable(): void {
+		this._finishStroke()
+		super.disable()
 	}
 
 	onMouseDown(pos: Position): void {
@@ -99,7 +114,14 @@ export class PenTool implements PaintTool {
 		this._pathPositions = undefined
 	}
 
-	private _constructCommand(): LayerDrawCommand {
+	protected abstract _constructCommand(): LayerDrawCommand
+}
+
+export class PenTool extends DrawingToolBase {
+	color: Color = '#ff0000'
+	width = 10
+
+	protected _constructCommand(): LayerDrawCommand {
 		return {
 			kind: 'stroke',
 			positions: this._pathPositions!,
@@ -109,90 +131,10 @@ export class PenTool implements PaintTool {
 	}
 }
 
-export class EraserTool implements PaintTool {
-	private _pathPositions: Position[] | undefined
+export class EraserTool extends DrawingToolBase {
 	width = 10
-	private readonly _imageCanvas: ImageCanvasDrawerWithPreview
-	private _targetLayerId: LayerId | undefined
 
-	private _isEnabled = false
-
-	constructor(private readonly _app: PaintApp) {
-		this._imageCanvas = _app.imageCanvas
-	}
-
-	enable(): void {
-		this._isEnabled = true
-	}
-
-	disable(): void {
-		this._finishStroke()
-		this._isEnabled = false
-	}
-
-	get isEnabled(): boolean {
-		return this._isEnabled
-	}
-
-	onMouseDown(pos: Position): void {
-		this._startStroke(pos)
-	}
-
-	onMouseMoved(pos: Position): void {
-		this._continueStroke(pos)
-	}
-
-	onMouseUp(_pos: Position): void {
-		this._finishStroke()
-	}
-
-	private _startStroke(pos: Position) {
-		if (this._pathPositions !== undefined) {
-			return
-		}
-
-		this._targetLayerId = this._app.layerManager.selectedLayerId
-		if (this._targetLayerId === undefined) {
-			return
-		}
-
-		this._pathPositions = [pos]
-		this._imageCanvas.startPreview(this._targetLayerId)
-	}
-
-	private _continueStroke({ x, y }: Position) {
-		if (this._pathPositions === undefined) {
-			return
-		}
-
-		const lastPosition = this._pathPositions[this._pathPositions.length - 1]
-		if (lastPosition.x === x && lastPosition.y === y) {
-			return
-		}
-
-		this._pathPositions.push({ x, y })
-		this._imageCanvas.drawPreview(this._constructCommand())
-		this._app.render()
-	}
-
-	private _finishStroke() {
-		if (this._pathPositions === undefined) {
-			return
-		}
-
-		this._imageCanvas.endPreview()
-		this._app.render()
-
-		this._app.commandSender.command({
-			kind: 'drawLayer',
-			layer: this._targetLayerId!,
-			drawCommand: this._constructCommand(),
-		})
-
-		this._pathPositions = undefined
-	}
-
-	private _constructCommand(): LayerDrawCommand {
+	protected _constructCommand(): LayerDrawCommand {
 		return {
 			kind: 'erase',
 			positions: this._pathPositions!,
@@ -201,35 +143,22 @@ export class EraserTool implements PaintTool {
 	}
 }
 
-export class MovingTool implements PaintTool {
-	private _isEnabled = false
-
+export class MovingTool extends PaintToolBase {
 	private _lastPos: Position | undefined
 	private _mouseMoveHandler: (e: MouseEvent) => void
 
 	constructor(private readonly _app: App) {
+		super()
 		this._mouseMoveHandler = (e) => this._onScrollContainerMouseMoved({ x: e.x, y: e.y })
-	}
-
-	enable(): void {
-		this._isEnabled = true
 	}
 
 	disable(): void {
 		this.onMouseUp()
-		this._isEnabled = false
-	}
-
-	get isEnabled(): boolean {
-		return this._isEnabled
+		super.disable()
 	}
 
 	onMouseDown(_pos: Position): void {
 		this._app.canvasScrollContainerElm.addEventListener('mousemove', this._mouseMoveHandler)
-	}
-
-	onMouseMoved(_pos: Position): void {
-		// pass
 	}
 
 	private _onScrollContainerMouseMoved(pos: Position): void {
