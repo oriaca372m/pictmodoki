043c39b93bf16b74857584a999e874f35e2632ea
oriaca <oriaca372m@outlook.jp>
2021-12-09 06:31:17 +0900

    出力動画サイズを半分に

--- server/src/drawing-recorder.ts
+++ server/src/drawing-recorder.ts
@@ -1,11 +1,20 @@
 import { Size } from 'common'
 import { Encoder } from 'encoder'
 
+function round2(x: number): number {
+	return Math.floor(x / 2) * 2
+}
+
 export class DrawingRecorder {
 	#encoder: Encoder
 
 	constructor(outputPath: string, readonly size: Size, framerate = 60) {
-		this.#encoder = new Encoder(outputPath, size, size, framerate)
+		this.#encoder = new Encoder(
+			outputPath,
+			size,
+			{ width: round2(size.width / 2), height: round2(size.height / 2) },
+			framerate
+		)
 		this.#encoder.init()
 	}
 
