89a0fc6678c52f6d3204b8f267f7538b27220223
oriaca <oriaca372m@outlook.jp>
2021-12-12 15:43:57 +0900

    グローバルcssの追加

--- client/src/views/index.vue
+++ client/src/views/index.vue
@@ -4,14 +4,38 @@
 	</div>
 </template>
 
-<script>
-</script>
-
 <style>
-#root-view {
+* {
 	margin: 0;
 	padding: 0;
+	box-sizing: border-box;
+}
+
+:root {
+	--color-primary: #63aeff;
+}
+
+#root-view {
 	width: 100%;
 	height: 100%;
+}
+
+.el_triangleUp {
+	width: 0;
+	height: 0;
+	border-style: solid;
+	border-width: 0 calc(var(--triangle-size) / 2) var(--triangle-size)
+		calc(var(--triangle-size) / 2);
+	border-color: transparent transparent var(--triangle-color) transparent;
+}
+
+.el_triangleDown {
+	width: 0;
+	height: 0;
+	border-style: solid;
+	border-color: transparent transparent var(--triangle-color) transparent;
+	border-width: var(--triangle-size) calc(var(--triangle-size) / 2) 0
+		calc(var(--triangle-size) / 2);
+	border-color: var(--triangle-color) transparent transparent transparent;
 }
 </style>
